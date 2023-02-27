bb339838c3a2bc9ff11ecac145db10869e5a29f1
daoraCode <severin.mboukou@gmail.com>
2023-02-27 15:18:12 +0100

    mongoose added

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
+import * as path from 'path'
+
 import express from "express"
 const app = express() 
+
+import mongoose from '../config/dbConfig.js'
 
 import * as dotenv from 'dotenv'
 dotenv.config()
 
 const port=process.env.PORT 
 
-app.listen(port, () => console.log(`application is running on port ${port}`) )
+app.listen(port, () => console.log(`application is running on port ${port}`))
