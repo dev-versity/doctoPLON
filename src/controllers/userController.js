818001c93b11076173c00c5e098d9a3d2d19288f
beuleu <billyklotz789@gmail.com>
2023-02-27 17:30:54 +0100

    working post register user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,26 +1,8 @@
 
-import User from "../models/UserModel"
-exports.register=async(req, res) => {
-    // we have the possibility to put the encryption here or in the model
+import User from "../models/UserModel.js"
 
-    try {
-        // const password = req.body.password
-        // TODO: add salt and hash password from bcrypt
-        // req.body.password = hashed password
-        const newUser= new User(req.body)
-        await newUser.save()
-        res.status(200).json({
-            message:"user created successfully",success: true
-        })
-    
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            message:"error creating user",success: false, error
-        })
-    }
-}
-exports.login= async(req, res) => {
+const login= async(req, res) => {
     
 }
 
+export default { register }
--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,9 +1,31 @@
-import {express, Router} from 'express'
-import {register,login} from "../controllers/userController"
+import {Router}  from 'express'
+import User      from "../models/UserModel.js";
 const userRouter = Router()
 
-userRouter.post('/register', register)
+userRouter.post('/register', async(req, res) => {
+  // we have the possibility to put the encryption here or in the model
+
+  try {
+    // const password = req.body.password
+    // TODO: add salt and hash password from bcrypt
+    // req.body.password = hashed password
+    const newUser= new User(req.body)
+    await newUser.save()
+    res.status(200).json({
+      message:"user created successfully",success: true
+    })
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message:"error creating user",success: false, error
+    })
+  }
+})
+
 
 userRouter.post('/login', (req, res) => {
-    // controller userControllers
+
 })
+
+export default userRouter
