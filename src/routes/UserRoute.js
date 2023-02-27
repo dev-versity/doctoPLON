0dd3fcb0776d75b04d39de93d423fcb5c61b7adc
nedys55 <denysyuksek@gmail.com>
2023-02-27 16:37:08 +0100

    UserRoute

---
+++ src/routes/UserRoute.js
@@ -0,0 +1,12 @@
+import {express, Router} from 'express'
+
+const userRouter = Router()
+
+userRouter.get('/register', (req, res) => {
+    // controller userControllers
+
+})
+
+userRouter.post('/login', (req, res) => {
+    // controller userControllers
+})
