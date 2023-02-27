56f1508d11caa04a98ddae982e553cdf5d253a7d
Hira Shahid <hira_shahid_26@hotmail.com>
2023-02-27 17:10:28 +0100

    register and login route

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,11 +1,8 @@
 import {express, Router} from 'express'
-
+import {register,login} from "../controllers/userController"
 const userRouter = Router()
 
-userRouter.get('/register', (req, res) => {
-    // controller userControllers
-
-})
+userRouter.post('/register', register)
 
 userRouter.post('/login', (req, res) => {
     // controller userControllers
